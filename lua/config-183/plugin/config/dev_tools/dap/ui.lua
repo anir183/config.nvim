2b7a6c94be71dd140a6544a7d46a077e2abd3e67
anir183 <188557785+anir183@users.noreply.github.com>
2026-02-15 00:54:30 +0530

    minor(dapui): dont auto close dap ui

--- lua/config-183/plugin/config/dev_tools/dap/ui.lua
+++ lua/config-183/plugin/config/dev_tools/dap/ui.lua
@@ -37,12 +37,12 @@ plugin.config = function(_, opts)
 	dap.listeners.before.launch.dapui_config = function()
 		dapui.open()
 	end
-	dap.listeners.before.event_terminated.dapui_config = function()
-		dapui.close()
-	end
-	dap.listeners.before.event_exited.dapui_config = function()
-		dapui.close()
-	end
+	-- dap.listeners.before.event_terminated.dapui_config = function()
+	-- 	dapui.close()
+	-- end
+	-- dap.listeners.before.event_exited.dapui_config = function()
+	-- 	dapui.close()
+	-- end
 end
 plugin.keys = {
 	{
