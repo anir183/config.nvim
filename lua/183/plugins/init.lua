109df0868ae44ac527a38d0560652ec4d8ea07be
anir183 <188557785+anir183@users.noreply.github.com>
2026-05-03 21:32:38 +0530

    feat(plugins): bootstrap lazy.nvim

---
+++ lua/183/plugins/init.lua
@@ -0,0 +1,62 @@
+--[[ setup plugin manager and install plugins ]]
+
+---@module "183.plugins.types"
+
+---@type 183.plugins.types.Module
+local M = {}
+
+M.install_cmd = {
+	"git",
+	"clone",
+	"--filter=blob:none",
+	"--branch=stable",
+	_G.CONSTS.lazy.repo_url,
+	_G.CONSTS.lazy.install_path,
+}
+
+function M.check_and_install()
+	if not (vim.uv or vim.loop).fs_stat(_G.CONSTS.lazy.install_path) then
+		local cmd_out = vim.fn.system(M.install_cmd)
+
+		if vim.v.shell_error ~= 0 then
+			LOG.error("failed to install lazy.nvim")
+			LOG.debug(cmd_out)
+			os.exit(1)
+		end
+
+		LOG.info("installed lazy.nvim successfully")
+	end
+end
+
+function M.init()
+	vim.opt.rtp:prepend(_G.CONSTS.lazy.install_path)
+
+	require("lazy").setup({
+		spec = { "nvim-lua/plenary.nvim" },
+		lockfile = _G.CONSTS.lazy.lock_file_path,
+		defaults = {
+			lazy = false,
+			priority = _G.CONSTS.lazy.priorities.default,
+		},
+		rocks = {
+			enabled = false,
+		},
+		checker = {
+			enabled = true,
+			notify = false,
+		},
+		change_detection = {
+			enabled = true,
+			notify = false,
+		},
+	})
+
+	_G.FUNCS.map(
+		"n",
+		"<leader>lz",
+		"<CMD>Lazy<CR>",
+		{ desc = "[plugin.lazy] open [L]a[Z]y" }
+	)
+end
+
+return M
---
+++ lua/183/plugins/types.lua
@@ -0,0 +1,6 @@
+--[[ plugins related types ]]
+
+---@class (exact) 183.plugins.types.Module
+---@field install_cmd? string[]
+---@field check_and_install? fun(): nil
+---@field init? fun(): nil
