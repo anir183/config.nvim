d3f0eda9577c9f5ee86cef40c586a7fda950f882
anir183 <188557785+anir183@users.noreply.github.com>
2026-05-05 22:04:27 +0530

    feat(plugins): add filtering keymaps to todo-comments plugin

--- lua/183/plugins/categories/quality_of_life/todo_comments.lua
+++ lua/183/plugins/categories/quality_of_life/todo_comments.lua
@@ -2,8 +2,6 @@
 
 ---@module "lazy"
 
--- BUG : commands like TodoQuickFix or Trouble todo only show NOTE comments
-
 ---@type LazySpec
 local plugin = {}
 
@@ -12,6 +10,44 @@ plugin.name = "todo-comments"
 plugin.dependencies = "plenary"
 plugin.lazy = false
 plugin.opts = {}
+plugin.config = function(_, opts)
+	require("todo-comments").setup(opts)
+	vim.defer_fn(function()
+		local local_find = {
+			ALL = function()
+				require("todo-comments.search").setloclist({
+					cwd = vim.fn.expand("%"),
+				})
+			end,
+		}
+		local global_find = {
+			ALL = function()
+				require("todo-comments.search").setqflist()
+			end,
+		}
+
+		for keyword, _ in
+			pairs(require("todo-comments.config").options.keywords)
+		do
+			local_find[keyword] = function()
+				require("todo-comments.search").setloclist({
+					keywords = keyword,
+					cwd = vim.fn.expand("%"),
+				})
+			end
+			global_find[keyword] = function()
+				require("todo-comments.search").setqflist({ keywords = keyword })
+			end
+		end
+
+		_G.FUNCS.mmap("<leader>td", local_find, {
+			desc = "[plugin.todo-comments] list [T]o[D]o comments",
+		})
+		_G.FUNCS.mmap("<leader>TD", global_find, {
+			desc = "[plugin.todo-comments] list [T]o[D]o comments",
+		})
+	end, 10)
+end
 plugin.keys = {
 	{
 		mode = "n",
